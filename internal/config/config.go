c6b4703d9517b76a41d0640c3fdafeeae473c965
Magnus Bengtsson <mbengtss@redhat.com>
2024-12-04 13:17:32 +0100

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,5 +3,5 @@ package config
 var (
 	DexImage       = "docker.io/dexidp/dex:v2.39.1-distroless"
 	GrafanaVersion = "9.5.17"
-	MariaDBImage   = "registry.access.redhat.com/rhel9/mariadb-1011:1-12"
+	MariaDBImage   = "registry.redhat.io/rhel9/mariadb-1011:1-12"
 )
